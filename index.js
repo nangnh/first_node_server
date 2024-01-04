ca2d052319ca1f0700fefa7fec89ed95da87873a
nangnh5 <nangnh@neo-lab.vn>
2024-01-04 16:12:22 +0700

    03_01_2024: add nodemon package, add some RESTFul API for note.

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 const http = require("http")
+const express = require("express");
 
+const app = express();
+
+/**
+ * Plugins
+ */
+// app.use(express.json())
+
+/**
+ * Data
+ */
 let notes = [
   {
     id: 1,
@@ -18,7 +29,8 @@ let notes = [
   }
 ]
 
-const app = http.createServer((request, response) => {
+/**Create app with http */
+const app1 = http.createServer((request, response) => {
   response.writeHead(200, {
     'Content-Type': 'text/plain',
     'Accept': 'application/json'
@@ -26,6 +38,58 @@ const app = http.createServer((request, response) => {
   response.end(JSON.stringify(notes))
 })
 
+/**Create app with express */
+/**
+ * GET COMMON
+ * */
+app.get('/', (request, response) => {
+  response.send('<h1>Hello world!!!</h1>')
+})
+
+/**
+ * GET all resources
+ * */
+app.get('/api/notes', (request, response) => {
+  response.json(notes)
+})
+
+/**
+ * GET a single resource
+ * */
+app.get('/api/notes/:id', (request, response) => {
+  const id = request.params.id
+  const note = notes.find(note => String(note.id) === String(id))
+
+  if (note) {
+    response.json(note)
+    return
+  }
+
+  response.status(404).end()  
+})
+
+/**
+ * DELETE a single resource
+ */
+
+app.delete('/api/notes/:id', (request, response) => {
+  const id = request.params.id
+  notes = notes.filter(note => String(note.id) !== String(id))
+
+  response.status(204).end()
+})
+
+/**
+ * Create a single resource
+ */
+app.post('/api/notes', (request, response) => {
+  const note = request.body
+  console.log('>note', note)
+
+  response.json(note)
+})
+
 const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
